bcfc3ef7848eb6ef6fc3e7dde93febef5477f4ba
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-05-27 23:38:55 -0300

    Update Container styles

---
+++ src/components/DonutChart/StyledComponents/Svg.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export default styled.svg`
+  border-radius: 50%;
+  display: block;
+  height: ${({height}) => height}px;
+  margin: 0 auto;
+  position: relative;
+  width: ${({width}) => width}px;
+
+  .donutchart-track{
+    fill: transparent;
+    stroke: #DAE2E5;
+    stroke-width: 26;
+  }
+
+  .donutchart-indicator {
+    fill: transparent;
+    stroke: #009688;
+    stroke-width: 26;
+    stroke-dasharray: 0 10000;
+    transition: stroke-dasharray .3s ease;
+  }
+
+  .donutchart-indicator-2 {
+    fill: transparent;
+    stroke: #d4c44e;
+    stroke-width: 26;
+    stroke-dasharray: 0 10000;
+    transition: stroke-dasharray .3s ease;
+  }
+
+  .donutchart-text{
+    fill: #607580;
+    font-family: 'Roboto';
+  }
+`;
--- src/components/DonutChart/StyledComponents/index.js
+++ src/components/DonutChart/StyledComponents/index.js
@@ -1,47 +1,11 @@
 import styled from 'styled-components';
+import Svg from './Svg';
+import InnerSvg from './InnerSvg';
 
 const Container = styled.svg`
-  height: ${({height}) => height}px;
-  margin: 0 auto;
-  border-radius: 50%;
-  display: block;
-  width: ${({width}) => width}px;
-
-  .donutchart-track{
-    fill: transparent;
-    stroke: #DAE2E5;
-    stroke-width: 26;
-  }
-
-  .donutchart-indicator {
-    fill: transparent;
-    stroke: #009688;
-    stroke-width: 26;
-    stroke-dasharray: 0 10000;
-    transition: stroke-dasharray .3s ease;
-  }
-
-  .donutchart-indicator-2 {
-    fill: transparent;
-    stroke: #d4c44e;
-    stroke-width: 26;
-    stroke-dasharray: 0 10000;
-    transition: stroke-dasharray .3s ease;
-  }
-
-  .donutchart-text{
-    font-family: 'Roboto';
-    fill: #607580;
-  }
-  .donutchart-text-val{
-    font-size:22px;
-  }
-  .donutchart-text-percent{
-    font-size:14px;
-  }
-  .donutchart-text-label{
-    font-size:9px;
-  }
 `;
+
+Container.Svg = Svg;
+Container.InnerSvg = InnerSvg;
 
 export default Container;
