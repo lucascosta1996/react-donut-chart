590201c4c8041fd94e7dcb038a90ebf5051d9f58
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-05-27 22:13:56 -0300

    Create DonutChart component

---
+++ src/components/DonutChart/DonutChart.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { number, string } from 'prop-types';
+import Container from './StyledComponents';
+
+function DonutChart({value, size, strokewidth}) {
+  const halfsize = (size * 0.5);
+  const radius = halfsize - (strokewidth * 0.5);
+  const circumference = 2 * Math.PI * radius;
+  const strokeval = ((value * circumference) / 100);
+  const stroke2val = ((77 * circumference) / 100);
+  const dashval = (strokeval + ' ' + circumference);
+  const dashval2 = (stroke2val + ' ' + circumference);
+
+  const trackstyle = {strokeWidth: strokewidth};
+  const indicatorstyle = {strokeWidth: strokewidth, strokeDasharray: dashval}
+  const indicator2style = {strokeWidth: strokewidth, strokeDasharray: dashval2}
+  const rotateval = 'rotate(-90 '+halfsize+','+halfsize+')';
+
+  return (
+    <Container width={size} height={size}>
+      <circle r={radius} cx={halfsize} cy={halfsize} transform={rotateval} style={trackstyle} className="donutchart-track"/>
+      <circle r={radius} cx={halfsize} cy={halfsize} transform={rotateval} style={indicator2style} className="donutchart-indicator-2"/>
+      <circle r={radius} cx={halfsize} cy={halfsize} transform={rotateval} style={indicatorstyle} className="donutchart-indicator"/>
+    </Container>
+  )
+};
+
+DonutChart.propTypes = {
+  value: number,
+  valuelabel: string,
+  size: number,
+  strokewidth: number
+};
+
+DonutChart.defaultProps = {
+  value: 0,
+  size: 116,
+  strokewidth: 10
+};
+
+export default DonutChart;
